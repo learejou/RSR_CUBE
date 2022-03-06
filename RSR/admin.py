75b300a6d4e89f08e365f4166e500c3bca52d67b
MPlantin <maxence.plantin@gmail.com>
2022-03-06 19:34:39 +0100

    Update admin.py

--- RSR/admin.py
+++ RSR/admin.py
@@ -3,3 +3,7 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 admin.site.register(Post)
+
+from .models import Commentaire
+# Register your models here.
+admin.site.register(Commentaire)
