24e690e31fb84c677a8b791829a9c4ef6bfd56a3
Arnaud Garing <arnaud.garing67@gmail.com>
2022-03-15 16:01:07 +0100

    Mise a jour page admin

--- Ressources/admin.py
+++ Ressources/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
 from .models import *
+
+
 # Register your models here.
 
+@admin.register(Ressources)
+class RessourcesAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at',)
+    list_display = ('titre', 'auteur', 'visits', 'created_at', 'updated_at')
+    list_filter = ('auteur', 'valide', 'created_at')
 
-admin.site.register(Ressources)
+
 admin.site.register(Consulte)
 admin.site.register(Commentaire)
 admin.site.register(Category)
